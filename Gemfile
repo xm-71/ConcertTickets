21cf9fed57e649b1010d747e3b288f4a70e96224
Alex Cannon <alexcannon2@live.com>
2017-04-24 16:00:34 -0400

    Added warning to concerttickets when tickets available is less than 20%

--- Gemfile
+++ Gemfile
@@ -5,6 +5,8 @@ gem 'authlogic'
 gem 'cancancan', '~> 1.10'
 
 gem 'activemerchant'
+
+gem 'responders', '~> 2.0'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.5'
 # Use sqlite3 as the database for Active Record
--- app/models/concert.rb
+++ app/models/concert.rb
@@ -3,4 +3,17 @@ class Concert < ActiveRecord::Base
   has_many :purchases
 
   belongs_to :venue
+
+  def tickets_remaining
+    a = self.tickets_available
+    b = self.purchases.sum(:ticket_quantity)
+    return a-b
+  end
+
+  def tickets_percentage
+    a = self.tickets_available
+    b = tickets_remaining
+    return (b.to_f / a.to_f) * 100
+  end
+
 end
